b522f2b37eddba98f4fefcb7f3f0563768ded03b
mjkchoi001 <103662994+mjkchoi001@users.noreply.github.com>
2024-10-31 20:13:16 -0400

    Update BankApplication.java

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -1,21 +1,17 @@
 import java.util.Scanner;
 
 public class BankApplication {
-
-
     public static void main(String[] args) {
-    	
-        Scanner scan =new Scanner(System.in);
-        System.out.println("Enter your 'Name' and 'CustomerId' to access your Bank account:");
-        String name=scan.nextLine();
-        String customerId=scan.nextLine();
-        
-        BankAccount obj1=new BankAccount(name,customerId);
-        
-        obj1.menu();
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your Name: ");
+        String name = scan.nextLine();
+        System.out.print("Enter your Customer ID: ");
+        String customerId = scan.nextLine();
+
+        BankAccount account = new BankAccount(name, customerId);
+        account.menu();
+
         scan.close();
-        
     }
 }
-
